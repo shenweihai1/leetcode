2794984afa6cd397608fb1da7fa58ff5c684df43
Weihai Shen <shenweihai1@gmail.com>
2018-12-24 23:54:40 +0800

    最近父节点

---
+++ lc_236.py
@@ -0,0 +1,31 @@
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        
+        if root == p or root == q:
+            return root
+        
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+
+        if l and r:
+            return root
+        
+        return l or (r or None)
+        
+        
