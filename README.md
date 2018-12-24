fa4fcd2fae220c909d8cf463a528d14e1b7162e2
Weihai Shen <shenweihai1@gmail.com>
2018-12-25 00:27:39 +0800

    Clone graph using DFS

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 ### 有意思
 1. Leetcode 236
 2. Leetcode 910 -- 从宏观思考问题
+
+### 注意代码
+1. Leetcode 133
---
+++ lc_133.py
@@ -0,0 +1,26 @@
+
+# Definition for a undirected graph node
+# class UndirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+# using DFS
+class Solution:
+    def __init__(self):
+        self.visited = {}
+
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if not node:
+            return None
+        if node in self.visited:
+            return self.visited[node]
+
+        root = UndirectedGraphNode(node.label)
+        self.visited[node] = root
+        for elem in node.neighbors:
+            root.neighbors.append(self.cloneGraph(elem))
+        return root
+        
