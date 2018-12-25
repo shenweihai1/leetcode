f02b1f7f6b22f6fc9e97a12fe4b85cbe6eafa673
Weihai Shen <shenweihai1@gmail.com>
2018-12-25 20:11:14 +0800

    Largest Number

---
+++ lc_179.py
@@ -0,0 +1,18 @@
+
+class Solution(object):
+    def largestNumber(self, nums):
+        nums = ["%s" % i for i in nums]
+        for i, m in enumerate(nums):
+            for j, n in enumerate(nums[i+1:]):
+                j = j + i + 1 # update the index of j
+                if "{f}{s}".format(f=m, s=n) < "{f}{s}".format(f=n, s=m):
+                    nums[i], nums[j] = n, m
+                    m = n
+        # special for 0
+        is_zero = True
+        for i in nums:
+            if i != '0':
+                is_zero = False
+        return "0" if is_zero else "".join(nums)
+
+        
