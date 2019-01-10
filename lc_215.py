5d60abef35a0c38ec91e5d4bf7f64215458cbc52
Weihai Shen <shenweihai1@gmail.com>
2019-01-09 23:59:23 -0700

    update

--- lc_215.py
+++ lc_215.py
@@ -18,3 +18,9 @@ class Solution:
                 heapq.heapreplace(heap, num)
         return heap[0]
         
+
+# the implemention of median by the statistics in the Cpython
+# https://github.com/python/cpython/blob/3.7/Lib/statistics.py
+
+# https://stackoverflow.com/questions/11068429/nth-element-implementations-complexities
+# O(N) ? https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained
