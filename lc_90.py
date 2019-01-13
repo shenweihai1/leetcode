72d208e0d91bb6b930a541f093aa0d9872cb7c6d
Weihai Shen <shenweihai1@gmail.com>
2019-01-12 20:49:41 -0700

    update

--- lc_90.py
+++ lc_90.py
@@ -12,17 +12,15 @@ class Solution:
         return list(ans) + [[]]
 
     # Sol2
-    def subsetsWithDup(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        count = collections.Counter(nums)
-        ans = [[]]
-        for n, c in count.items():
-            tmp = []
-            for prev in ans:
-                for curr in [[n] * i for i in range(c + 1)]:
-                    tmp.append(prev + curr)
-            ans = tmp
-        return ans
+    # def subsetsWithDup(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: List[List[int]]
+    #     """
+    #     count = collections.Counter(nums)
+    #     ans = [[]]
+    #     for n, c in count.items():
+    #         for i in range(len(ans)):
+    #             for curr in [[n] * i for i in range(1, c + 1)]:
+    #                 ans.append(ans[i] + curr)
+    #     return ans
