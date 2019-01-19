4323972be82037e6220b825722c91c81bbc74e70
Weihai Shen <shenweihai1@gmail.com>
2019-01-19 08:32:17 -0700

    update

--- tree/13_target_path.py
+++ tree/13_target_path.py
@@ -1,14 +1,25 @@
-
 #-*- coding:utf-8
-class Solution:
-    def targetPath(self, root, target):
-        pass
-
-
-if __name__ == "__main__":
-    from binarytree import bst
-    import random
-    root = bst(height=3)
-    obj = Solution()
-    print(obj.targetPath(root))
-    print(root)
+# 主要思想：遍历到叶子节点并且sum减去每个节点的value等于0即为目标path
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        ans, path = [], []
+        
+        def helper(root, sum, path):
+            if not root: return
+            if sum == root.val and not root.left and not root.right:
+                path.append(root.val)
+                ans.append([i for i in path])
+                path.pop()
+            
+            path.append(root.val)
+            helper(root.right, sum - root.val, path)
+            helper(root.left, sum - root.val, path)
+            path.pop()
+        
+        helper(root, sum, path)
+        return ans
