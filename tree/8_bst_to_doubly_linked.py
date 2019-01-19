cc7fbff5d1f942c6bb3d216725c86ac5928c9a3c
Weihai Shen <shenweihai1@gmail.com>
2019-01-19 08:47:43 -0700

    update

--- tree/8_bst_to_doubly_linked.py
+++ tree/8_bst_to_doubly_linked.py
@@ -1,17 +1,16 @@
 #-*- coding:utf-8
+# 具体题目描述: https://segmentfault.com/a/1190000016762566
+# FIXME
 class Solution(object):
 
     def bstToDoublyLinked(self, root):
         pass
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     obj = Solution()
 
     from binarytree import bst
-    import random
-    height = random.randint(3, 4)
-    root = bst(height=height)
+    root = bst(height=2)
     obj = Solution()
-    
-
+    print(root)
     print(obj.bstToDoublyLinked(root))
