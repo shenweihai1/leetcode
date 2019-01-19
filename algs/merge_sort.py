8c2fc943a6db190a345d7cc5d1bb9b2eac5aed4e
Weihai Shen <shenweihai1@gmail.com>
2019-01-19 14:08:42 -0700

    merge sort

---
+++ algs/merge_sort.py
@@ -0,0 +1,27 @@
+#-*- coding:utf-8
+from heapq import merge
+import bisect
+
+class Solution:
+    def mergeSort(self, nnums):
+        N = len(nnums)
+        if N <= 1:
+            return nnums
+        return list(merge(self.mergeSort(nnums[0:N/2]), self.mergeSort(nnums[N/2:])))
+
+    def mergeSortAdv(self, nnums):
+        if len(nnums) <= 1:
+            return []
+
+        ans, rest = [nnums[0]], nnums[1:]
+        while rest:
+            u = rest.pop()
+            idx = bisect.bisect_right(ans, u)
+            ans = ans[0:idx] + [u] + ans[idx:]
+        return ans
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.mergeSort([1,2.1,31,4,5,6,7,0]))
+    print(obj.mergeSortAdv([1,2.1,31,4,5,6,7,0]))
