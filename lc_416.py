1a7461f782b1a6a2b08dca0eb79ca33c5fa07670
Weihai Shen <shenweihai1@gmail.com>
2019-01-25 22:14:02 -0700

    lc_416, dp

--- lc_416.py
+++ lc_416.py
@@ -12,7 +12,7 @@ class Solution:
         dp = [False] * (target + 1)
         dp[0] = True
         
-        for i in range(N):
+        for i in range(1, N):
             for j in range(target, nums[i] - 1, -1): 
                 dp[j] = dp[j] or dp[j - nums[i]]
         return dp[target]
