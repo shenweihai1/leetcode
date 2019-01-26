65630d77917dbe636600e860ce504e2511c0886f
Weihai Shen <shenweihai1@gmail.com>
2019-01-25 21:15:59 -0700

    update

---
+++ lc_416.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        target = sum(nums)
+        if target & 1 == 1: return False
+        target = target >> 1
+        N = len(nums)
+        
+        dp = [False] * (target + 1)
+        dp[0] = True
+        
+        for i in range(N):
+            for j in range(target, nums[i] - 1, -1): 
+                dp[j] = dp[j] or dp[j - nums[i]]
+        return dp[target]
