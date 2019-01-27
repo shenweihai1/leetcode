d9ef588b035391381e117485980555e5f80c524f
Weihai Shen <shenweihai1@gmail.com>
2019-01-27 00:26:45 -0700

    another dp

--- algs/mini_sum_two_subarray.py
+++ algs/mini_sum_two_subarray.py
@@ -1,10 +1,11 @@
 
 class Solution:
     # time complexity: O(2 ^ n)
+    # optimize, avoiding overlapping
     def find_mini(self, A):
         def helper(idx, target):
             if idx == len(A):
-                if abs(2 * target - sum(A)) < ans[0]:
+                if abs(2 * target - S) < ans[0]:
                     ans[0] = abs(2 * target - S)
                 return
 
@@ -26,9 +27,24 @@ class Solution:
                 dp[j] = max(dp[j], dp[j - num] + num)
         return abs(dp[-1] * 2 - sum(A))
 
+    def another_dp(self, A):
+        target = sum(A) // 2
+        dp = [0] * (target + 1)  # dp[0] => dp[1] => dp[2] ... dp[len(A) - 1]
+        for i in range(len(A)):
+            for j in range(target, -1, -1):
+                if i == 0:
+                    dp[j] = A[i] if j >= A[i] else 0
+                    continue
+
+                if j >= A[i]:
+                    dp[j] = min(dp[j - A[i]] + A[i], dp[j])
+        
+        return dp[-1]
+
 
 if __name__ == "__main__":
     A = [1, 6, 11, 5, 2, 4, 2, 23, 4, 9, 32, 13, 6, 1, 1, 1, 1, 1, 42, 32]
     obj = Solution()
     print(obj.find_mini(A))
     print(obj.find_mini_dp(A))
+    print(obj.another_dp(A))
