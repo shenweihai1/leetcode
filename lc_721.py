8b8989b53e38848c4395c05e0d2729d30a9c5f58
Weihai Shen <wshen24@asu.edu>
2020-02-22 14:11:00 -0700

    updated for 721

--- lc_721.py
+++ lc_721.py
@@ -1,39 +1,35 @@
 class UnionFind(object):
     def __init__(self, ns):  # nodes
         self.father = {n: n for n in ns}
-    
+
     def union(self, u, v):
         self.father[self.find(u)] = self.find(v)
-    
+
     def find(self, u):
         if u == self.father[u]:
             return u
-        
+
         self.father[u] = self.find(self.father[u])
         return self.father[u]
-    
+
 class Solution:
     def accountsMerge(self, A):
         """
         :type accounts: List[List[str]]
         :rtype: List[List[str]]
         """
-        import collections        
-
-        # (username, idx): emails
-        idx_acts = {(es[0], idx): es[1:] for idx, es in enumerate(A)}
-        uf = UnionFind(idx_acts.keys())
+        # email:idx
+        e_i = {email: index for index, p in enumerate(A) for email in p[1:]}
+        # UF(emails)
+        UF = UnionFind(e_i.keys())
+        # union within same group
+        {UF.union(p[i], p[i + 1])  for p in A for i in range(1, len(p[1:]))}
         
-        # email => [(username, idx)]
-        inv_acts = collections.defaultdict(list)
-        for u, emails in idx_acts.items():
-            [uf.union(u, e) for email in emails for e in inv_acts.get(email, [])]
-            [inv_acts[email].append(u) for email in emails]
-
-        # merge connected accounts
-        # (username, idx): set([email])
+        # merge into index:emails
+        import collections
         ans = collections.defaultdict(set)
-        for u, es in idx_acts.items():
-            ans[uf.find(u)] |= set(es)
-
-        return [[k[0]] + sorted(list(v)) for k, v in ans.items()]
+        
+        for p in A:
+            ans[e_i[UF.find(p[1])]] |= set(p[1:])
+            
+        return [[A[k][0]] + sorted(list(v)) for k, v in ans.items()]
