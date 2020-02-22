d6a14afe805093208c3e96b2e4eaaac0221ecb85
Weihai Shen <wshen24@asu.edu>
2020-02-22 00:40:07 -0700

    Updated

--- algs/maxsum_continguous_subarray.py
+++ algs/maxsum_continguous_subarray.py
@@ -1,15 +1,15 @@
 
-def maxSubArraySum(A): 
-    cons = 0
-    ans = min(A)
-    for ele in A:
-        cons = max(0, cons + ele)
-        ans = max(ans, cons)
+def maxSubArraySumDP(A):
+    dp = [A[0], A[0]]
+    ans = A[0]
+    for i in range(1, len(A) - 1):
+        dp[i % 2] = dp[(i - 1) % 2] + A[i] if dp[(i - 1) % 2] >= 0 else A[i]
+        ans = max(dp)
     return ans
-
    
 # Driver function to check the above function  
-A = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, -7] 
-A = [-2, -3, 4, -1, -2, 1, 5, -3]
-print("Maximum contiguous sum is: %s" % maxSubArraySum(A))
+A = [-13, -3, -25]
+B = [-2, -3, 4, -1, -2, 1, 5, -3]
+print("Maximum contiguous sum is: %s" % maxSubArraySumDP(A))
+print("Maximum contiguous sum is: %s" % maxSubArraySumDP(B))
    
