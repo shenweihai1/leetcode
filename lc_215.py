575483e737e1f50d552e411b804c3dac15f85e02
Weihai Shen <wshen24@asu.edu>
2020-02-23 17:12:21 -0700

    update

--- lc_215.py
+++ lc_215.py
@@ -1,4 +1,42 @@
 
+class SolutionA(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        A = nums
+        if not A or k < 1 or k > len(A):
+            return None
+        return self.partition(A, 0, len(A) - 1, len(A) - k)
+        
+    def partition(self, nums, start, end, k):
+        """
+        During the process, it's guaranteed start <= k <= end
+        """
+        if start == end:
+            return nums[k]
+            
+        left, right = start, end
+        pivot = nums[(start + end) // 2]
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] > pivot:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left, right = left + 1, right - 1
+                
+        # left is not bigger than right
+        if k <= right:
+            return self.partition(nums, start, right, k)
+        if k >= left:
+            return self.partition(nums, left, end, k)
+        
+        return nums[k]
+
 class Solution:
     def findKthLargest(self, nums, k):
         """
@@ -15,12 +53,37 @@ class Solution:
         heapq.heapify(heap)  # heappush, heappop
         for num in nums[k:]:
             if heap[0] < num:
-                heapq.heapreplace(heap, num)
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
         return heap[0]
+
+# https://www.jiuzhang.com/solution/quick-sort/#tag-highlight-lang-python
+class SolutionB:
+    # @param {int[]} A an integer array
+    # @return nothing
+    def sortIntegers2(self, A):
+        # Write your code here
+        self.quickSort(A, 0, len(A) - 1)
+    
+    def quickSort(self, A, start, end):
+        if start >= end:
+            return
         
+        left, right = start, end
+        # key point 1: pivot is the value, not the index
+        pivot = A[(start + end) // 2]
 
-# the implemention of median by the statistics in the Cpython
-# https://github.com/python/cpython/blob/3.7/Lib/statistics.py
-
-# https://stackoverflow.com/questions/11068429/nth-element-implementations-complexities
-# O(N) ? https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60294/Solution-explained
+        # key point 2: every time you compare left & right, it should be 
+        # left <= right not left < right
+        while left <= right:
+            while left <= right and A[left] < pivot:
+                left += 1
+            while left <= right and A[right] > pivot:
+                right -= 1
+            if left <= right:
+                A[left], A[right] = A[right], A[left]
+                left += 1
+                right -= 1
+        
+        self.quickSort(A, start, right)
+        self.quickSort(A, left, end)
