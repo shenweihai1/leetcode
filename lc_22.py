f10169cc427adf5b883723e871a1dae9f8265bc3
Weihai Shen <wshen24@asu.edu>
2020-02-23 19:17:11 -0700

    Updated

--- lc_22.py
+++ lc_22.py
@@ -17,3 +17,29 @@ class Solution:
             dp.append(set(list(ans)))
         return list(dp[n])
         
+    
+class SolutionA(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+        def dfs(l, r, prefix):
+            # always r >= l
+            if l > r or l < 0 or r < 0: return
+            
+            if l == 0 and r == 0:
+                ans.append(prefix)
+                return
+            
+            dfs(l, r - 1, prefix + ")")
+            dfs(l - 1, r, prefix + "(")
+        
+        dfs(n, n, "")
+        return ans
+
+
+if __name__ == "__main__":
+    obj = SolutionA()
+    print(obj.generateParenthesis(4))
--- lc_921.py
+++ lc_921.py
@@ -14,3 +14,22 @@ class Solution(object):
             if l < 0:
                 r, l = r + 1, 0
         return l + r
+
+
+class SolutionA(object):
+    def minAddToMakeValid(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        stack = []
+        for s in S:
+            if s == ")":
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    stack.append(")")
+            elif s == "(":
+                stack.append("(")
+                
+        return len(stack)
